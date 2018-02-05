df9b1b9a42a71531c0752b58d444b8e6e0061a99
Jason Baker <jasonstephenbaker@gmail.com>
2018-02-05 17:45:39 -0500

    Testing state and update pattern

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,24 +3,44 @@ require('styles/App.css');
 
 import React from 'react';
 import io from 'socket.io-client';
+// import StatusControl from './StatusControl';
 
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			printerMsg: '',
+			buttonType: '',
+		};
 
-  constructor() {
-  	super();
-    this.socket = io('ws://localhost:8080');
-    this.socket.on('message', function(message) {
-      console.log(message);
-    });
-  }
+		this.socket = io('ws://localhost:8080');
+		this.socket.on('message', (message) => {
+			console.log(message);
+			this.setState({printerMsg: JSON.stringify(message)});
+			this.setButtonType(message);
+		});
+	}
+
+	setButtonType = msg => {
+
+		if (msg.status.state === 'Idle')
+			this.setState({buttonType: 'PRINT'});
+	};
+
+	sendAction = () => {
+		this.socket.send(this.state.buttonType);
+	};
 
   render() {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <br/>
+        <h1>{ this.state.printerMsg }</h1>
+        <button onClick={this.sendAction}>{this.state.buttonType}</button>
       </div>
     );
   }
